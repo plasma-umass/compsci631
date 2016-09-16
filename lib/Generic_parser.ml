643ad3e238dd3007e9955b7fde4a2ac76e955659
Arjun Guha <arjun@cs.umass.edu>
2016-09-16 07:04:11 -0400

    Fixed identifier parser
    Reserved words like "then", "else", and "in" were getting parsed as identifiers
    when parsing applications.

--- lib/Generic_parser.ml
+++ lib/Generic_parser.ml
@@ -4,7 +4,16 @@ open Tokens
 
 type 'a parser = ('a, unit) MParser.t
 
-let id : string parser = regexp (make_regexp "[A-Za-z_][A-Za-z_0-9_']*") <<< spaces
+(* Keywords that could be confused for identifiers. *)
+let reserved_words = [
+    "true"; "false"; "empty"; "head"; "tail"; "empty?"; "if"; "then";
+    "else"; "let"; "in"; "fun"; "fix"
+  ]
+
+let id : string parser =
+  attempt (((choice (List.map symbol reserved_words)) >>=
+     fun x -> fail ("unexpected " ^  x ^ " (reserved word)")) <|>
+  regexp (make_regexp "[A-Za-z_][A-Za-z_0-9_']*") <<< spaces)
 
 let rev_fold_left f xs = match List.rev xs with
   | [] -> raise (Failure "expected at least one element (internal error)")
--- tests/Interp_util_tests.ml
+++ tests/Interp_util_tests.ml
@@ -7,3 +7,19 @@ let%TEST "normal subtraction associaticity" =
 let%TEST "a list of three integers" =
   from_string "1 :: 2 :: 3 :: empty" =
     Cons(Const (Int 1), Cons (Const (Int 2), Cons (Const (Int 3), Empty)))
+
+let%TEST "a list of three integers" =
+  from_string "1 :: 2 :: 3 :: empty" =
+    Cons(Const (Int 1), Cons (Const (Int 2), Cons (Const (Int 3), Empty)))
+
+let%TEST "Conrad\'s example" =
+  from_string "let f = (fun x -> 1) in 1" =
+    Let ("f", Fun ("x", Const (Int 1)), Const (Int 1))
+
+let%TEST "Harsh example 1" =
+  from_string "if true then 700 else 600" =
+    If (Const (Bool true), Const (Int 700), Const (Int 600))
+
+let%TEST "Harsh example 2" =
+  from_string "let x = 700 in x" =
+    Let ("x", Const (Int 700), Id "x")
